557a1e347d35a08b16d3a9d8d7785161e13e4de9
bhara <bharathJ222000@gmail.com>
2020-11-08 22:12:21 +0530

    added

---
+++ controllers/auth.js
@@ -0,0 +1,22 @@
+const User = require('../models/user');
+
+//  Create user without email account activation
+exporrts.signup = (req, res) => {
+    console.log(req.body);
+    const { name, email, password } = req.body;
+    User.findOne({ email }).exec((err, user) => {
+        if (user) {
+            return res.status(400).json({ error: "User with this emailalready exists." });
+        }
+        let newUser = new User({ name, email, password });
+        newUser.save((err, success) => {
+            if (err) {
+                console.log('Error in signup: ', err);
+                return res.status(400), json({ error: err })
+            }
+            res.json({
+                message: "Signup success!!"
+            })
+        })
+    });
+}
