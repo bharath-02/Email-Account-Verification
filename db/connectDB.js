6b28240d7579a48fb909faba44afaeadd23ad3fd
bhara <bharathJ222000@gmail.com>
2020-11-08 19:51:35 +0530

    added

---
+++ db/connectDB.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+// Connect to DB
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(() => {
+    console.log('DB connection established');
+}).catch((err) => {
+    console.log('DB connection error: ', err);
+})
